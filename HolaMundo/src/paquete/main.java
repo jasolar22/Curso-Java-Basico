33de14e5ce38e4d7a6649762db3e3bc7e889106e
jasolar22 <38046915+jasolar22@users.noreply.github.com>
2018-04-03 11:26:28 -0500

    Add files via upload

---
+++ HolaMundo/src/paquete/main.java
@@ -0,0 +1,10 @@
+package paquete;
+
+public class main {
+
+    public static void main(String[] args) {
+        System.out.printf("Hola Mundo Java \n");
+        System.out.printf("Bienvenidos...");
+    }
+
+}
