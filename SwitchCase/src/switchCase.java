19c2f03d8fa7940d291cf3ec5ebd88c507887b3a
jasolar22 <38046915+jasolar22@users.noreply.github.com>
2018-04-03 18:06:40 -0500

    Add files via upload

---
+++ SwitchCase/src/switchCase.java
@@ -0,0 +1,27 @@
+public class switchCase {
+
+    public static void main(String[] args) {
+
+        int option=1;
+
+        switch (option)
+        {
+            case 0:
+            {
+                System.out.println("Este es el caso 0");
+                break;
+            }
+            case 1:
+            {
+                System.out.println("Este es el caso 1");
+                break;
+            }
+            case 2:
+            {
+                System.out.println("Este es el caso 2");
+                break;
+            }
+
+        }
+    }
+}
